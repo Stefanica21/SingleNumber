dc55155e34995a35846323ad880071b04d288955
Stefanica21 <nica.stefana21@gmail.com>
2024-08-11 15:49:28 +0300

    Add solution to the repository

---
+++ SingleNumber/SingleNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SingleNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter sequence of numbers separated by white space");
+            var input = Console.ReadLine();
+            try
+            {
+                int[] intArray = Array.ConvertAll(input.Split(' '), s => int.Parse(s));
+                int result = Utils.GetSingleNumber(intArray);
+                if (result == -1) Console.WriteLine("Invalid array");
+                else Console.WriteLine("Single number is: " + result);
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid array");
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
---
+++ SingleNumber/SingleNumber/Utils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SingleNumber
+{
+    public static class Utils
+    {
+        public static int GetSingleNumber(int[] array)
+        {
+            if (array.Length % 2 == 0 || array == null) return -1;
+
+            Array.Sort(array);
+
+            for(int i=0;i<array.Length; i = i + 2)
+            {
+                if (array[i] != array[i + 1])
+                    return array[i];
+
+                if (i == array.Length - 1) return array[i + 1];
+            }
+
+            return -1;
+        }
+    }
+}
